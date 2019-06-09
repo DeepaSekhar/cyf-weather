4371872d048462df10f7c493742606f9b55d1e4d
deepaakhil84 <deepaakhiltvm@gmail.com>
2019-06-09 14:02:46 +0100

    created page and styled it

--- src/App.js
+++ src/App.js
@@ -10,8 +10,12 @@ class App extends Component {
   render() {
     return (
       <div className="app">
-        <header className="app__header" />
-
+        <header className="app__header">
+          <div className="searchBox">
+            <textarea className="input">London</textarea>
+          </div>
+          <button className="submit-button">Submit</button>
+        </header>
         <main className="app__main">
           <h1>DEEPA AKHIL</h1>
         </main>
