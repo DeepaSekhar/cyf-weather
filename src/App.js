eaa5d4bd9f95b64b9c8fc548b18f8fc218e44899
deepaakhil84 <deepaakhiltvm@gmail.com>
2019-06-09 16:35:06 +0100

    added more page content

--- src/App.js
+++ src/App.js
@@ -13,11 +13,95 @@ class App extends Component {
         <header className="app__header">
           <div className="searchBox">
             <textarea className="input">London</textarea>
+            <button className="submit-button">Submit</button>
           </div>
-          <button className="submit-button">Submit</button>
         </header>
         <main className="app__main">
-          <h1>DEEPA AKHIL</h1>
+          <div>
+            <div className="main-data">
+              <img
+                className="main-image"
+                src="https://placekitten.com/g/200/300"
+                alt=" Clouds Overcast"
+              />
+            </div>
+            <div className="detail">
+              <div>
+                <h4>overcast clouds</h4>
+                <h3>Temperature 10* to 11*</h3>
+                <h5>Humidity 78% Pressure 1008 48</h5>
+              </div>
+
+              <div className="container">
+                <div>
+                  <h5>3.00</h5>
+                  <img
+                    className="sub-image"
+                    src="https://placekitten.com/200/300"
+                    alt="weather"
+                  />
+                  <h5>8*C</h5>
+                </div>
+
+                <div>
+                  <h5>3.00</h5>
+                  <img
+                    className="sub-image"
+                    src="https://placekitten.com/200/300"
+                    alt=""
+                  />
+                  <h5>8*C</h5>
+                </div>
+
+                <div>
+                  <h5>3.00</h5>
+                  <img
+                    className="sub-image"
+                    src="https://placekitten.com/200/300"
+                    alt=""
+                  />
+                  <h5>8*C</h5>
+                </div>
+                <div>
+                  <h5>3.00</h5>
+                  <img
+                    className="sub-image"
+                    src="https://placekitten.com/200/300"
+                    alt=""
+                  />
+                  <h5>8*C</h5>
+                </div>
+
+                <div>
+                  <h5>3.00</h5>
+                  <img
+                    className="sub-image"
+                    src="https://placekitten.com/200/300"
+                    alt=""
+                  />
+                  <h5>8*C</h5>
+                </div>
+                <div>
+                  <h5>3.00</h5>
+                  <img
+                    className="sub-image"
+                    src="https://placekitten.com/200/300"
+                    alt=""
+                  />
+                  <h5>8*C</h5>
+                </div>
+                <div>
+                  <h5>3.00</h5>
+                  <img
+                    className="sub-image"
+                    src="https://placekitten.com/200/300"
+                    alt=""
+                  />
+                  <h5>8*C</h5>
+                </div>
+              </div>
+            </div>
+          </div>
         </main>
       </div>
     );
